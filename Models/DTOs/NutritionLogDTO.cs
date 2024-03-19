5b6e357841cc1d1c257957ef5433941a6131e854
Idris Ismail <123032071+IdrisIsmail1994@users.noreply.github.com>
2024-03-19 11:48:04 +0100

    Idrisi (#2)
    * Implemenert kode i NutritionLog
    
    * Endret navn på til NutritionResourcesDTO
    
    * tvungen

--- Models/DTOs/NutritionLogDTO.cs
+++ Models/DTOs/NutritionLogDTO.cs
@@ -1,5 +1,24 @@
 ﻿namespace FitnessPartner.Models.DTOs
 {
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> main
+    public class NutritionLogDTO
+    {
+
+        public NutritionLogDTO(int logId, int userId, DateTime date, string foodIntake)
+        {
+            LogId = logId;
+            UserId = userId;
+            Date = date;
+            FoodIntake = foodIntake;
+        }
+
+        public int LogId { get; set; }
+<<<<<<< HEAD
+=======
+=======
 	public class NutritionLogDTO
 	{
 		public NutritionLogDTO(int logId, int userId, DateTime date, string foodIntake)
@@ -11,6 +30,8 @@
 		}
 
 		public int LogId { get; set; }
+>>>>>>> 55c9c0e1477aa35f17748aec687e9dd873e00a69
+>>>>>>> main
         public int UserId { get; set; }
         public DateTime Date { get; set; }
         public string FoodIntake { get; set; } = string.Empty;
--- Models/Entities/NutritionLog.cs
+++ Models/Entities/NutritionLog.cs
@@ -1,8 +1,33 @@
 ﻿using System.ComponentModel.DataAnnotations;
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> main
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitnessPartner.Models.Entities
+{
+    public class NutritionLog
+    {
+        [Key]
+        public int LogId { get; set; }
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public string FoodIntake { get; set; } = string.Empty;
+
+    }
+<<<<<<< HEAD
+=======
+=======
 
 namespace FitnessPartner.Models.Entities
 {
 	public class NutritionLog
 	{
 	}
+>>>>>>> 55c9c0e1477aa35f17748aec687e9dd873e00a69
+>>>>>>> main
 }
