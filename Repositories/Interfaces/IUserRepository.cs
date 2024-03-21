af846fbac0b6603ce0ca49de3164702bb1b004e8
Idris Ismail <Idris10B@hotmail.com>
2024-03-21 12:49:09 +0100

    aaaanyy

--- Repositories/Interfaces/IUserRepository.cs
+++ Repositories/Interfaces/IUserRepository.cs
@@ -4,5 +4,7 @@ namespace FitnessPartner.Repositories.Interfaces;
 
 public interface IUserRepository
 {
-	
+    Task<User> AddUserAsync(User user);
+    Task<User> UpdateUserAsync(User user);
+    Task<User> DeleteUserByIdAsync(User user);
 }
